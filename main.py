14c248f233e4e85679403984064d08bad2d0c193
Kris <kris.weber.1994@gmail.com>
2017-10-30 11:06:19 -0700

    Add list to hold all words in file

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ def cli():
     specific_file.close()
     unique_words(file_text)
 
-def line_to_words(text):
-    text = text.split(" ")
-    return text
-
 def unique_words(text):
     if len(text) == 0:
         return
 
-    curr_count = 0
+    all_words = list()
 
+    for line in text:
+        line = line.split()
+        for word in line:
+            all_words.append(word)
+
+            
+
+
+    curr_count = 0
 
 
 if __name__ == '__main__':
